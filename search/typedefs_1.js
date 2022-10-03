8b9e5448b19d47e7bb86f815f14c2d2ab3b4aed2
pskelton <pskelton@users.noreply.github.com>
2022-10-03 20:43:44 +0000

    Deploying to gh-pages from @ pskelton/world-of-might-and-magic@610835c7e19f9c6be93ff3b5dd97b49ab34876c3 🚀

--- search/typedefs_1.js
+++ search/typedefs_1.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['bbox_5ffloat_5f_0',['BBox_float_',['../VectorTypes_8h.html#ad3d637b4b10fac5ad00af2c8dfd34173',1,'VectorTypes.h']]],
-  ['bbox_5fint_5f_1',['BBox_int_',['../VectorTypes_8h.html#a65980ab94e05bb30e4eaad82dfbb719a',1,'VectorTypes.h']]],
-  ['bbox_5fshort_5f_2',['BBox_short_',['../VectorTypes_8h.html#ab9df63db613d1c6a5f50e2b7889925f8',1,'VectorTypes.h']]],
-  ['bstreamer_3',['bstreamer',['../PCX_8cpp.html#ac7f304b78511b342bef37de230ef6823',1,'PCX.cpp']]]
+  ['base_5freference_0',['base_reference',['../classdetail_1_1IndexedArrayMapIterator.html#a792e47a69f25a33480887cc8371fa354',1,'detail::IndexedArrayMapIterator']]],
+  ['base_5ftype_1',['base_type',['../classIndexedArray.html#a634a30d24a952764b65584fb06d42c36',1,'IndexedArray']]],
+  ['bboxf_2',['BBoxf',['../VectorTypes_8h.html#ab5b6bfa64694c7701e1d768df5e394f0',1,'VectorTypes.h']]],
+  ['bboxi_3',['BBoxi',['../VectorTypes_8h.html#a2a198f50ad86b7c20980d1364f0903d6',1,'VectorTypes.h']]],
+  ['bboxs_4',['BBoxs',['../VectorTypes_8h.html#a033222fae543fd9d618c62cd4f42a37a',1,'VectorTypes.h']]],
+  ['bstreamer_5',['bstreamer',['../PCX_8cpp.html#ac7f304b78511b342bef37de230ef6823',1,'PCX.cpp']]]
 ];
--- search/typedefs_c.js
+++ search/typedefs_c.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['validator_5ftype_0',['validator_type',['../classApplication_1_1GameConfig_1_1ConfigValue.html#aad290fbe1dbcb13ee57ec2c65c35492b',1,'Application::GameConfig::ConfigValue']]],
-  ['vec2_5ffloat_5f_1',['Vec2_float_',['../VectorTypes_8h.html#adab72640f2072bb65c5ad21b549a12bb',1,'VectorTypes.h']]],
-  ['vec2_5fint_5f_2',['Vec2_int_',['../VectorTypes_8h.html#abfba0cb2e54eb0d70d6277cefed19841',1,'VectorTypes.h']]],
-  ['vec3_5ffloat_5f_3',['Vec3_float_',['../VectorTypes_8h.html#acfaf1fc0da7a735f6eab18ef0d39e7bc',1,'VectorTypes.h']]],
-  ['vec3_5fint64_5f_4',['Vec3_int64_',['../VectorTypes_8h.html#abe8249ac8fb261fa383a5c9dbea53eca',1,'VectorTypes.h']]],
-  ['vec3_5fint_5f_5',['Vec3_int_',['../VectorTypes_8h.html#a280a4605b33cb88d3542ac14e433315b',1,'VectorTypes.h']]],
-  ['vec3_5fshort_5f_6',['Vec3_short_',['../VectorTypes_8h.html#a5b041ecb847c6c3177bd0dd4f4f827d3',1,'VectorTypes.h']]],
-  ['vis_5fobject_7',['Vis_Object',['../Vis_8h.html#a901e04389cae1cad859b1597966b7bc3',1,'Vis.h']]]
+  ['uchar_0',['uchar',['../MM7_8h.html#a65f85814a8290f9797005d3b28e7e5fc',1,'MM7.h']]],
+  ['uint_1',['uint',['../MM7_8h.html#a91ad9478d81a7aaf2593e8d9c3d06a14',1,'MM7.h']]],
+  ['uint16_2',['uint16',['../MM7_8h.html#a05f6b0ae8f6a6e135b0e290c25fe0e4e',1,'MM7.h']]],
+  ['uint32_3',['uint32',['../MM7_8h.html#a1134b580f8da4de94ca6b1de4d37975e',1,'MM7.h']]],
+  ['uint64_4',['uint64',['../MM7_8h.html#a20e3bd7609c6ce94e9bcd863f8e7113b',1,'MM7.h']]],
+  ['uint8_5',['uint8',['../MM7_8h.html#adde6aaee8457bee49c2a92621fe22b79',1,'MM7.h']]],
+  ['ulong_6',['ulong',['../MM7_8h.html#a718b4eb2652c286f4d42dc18a8e71a1a',1,'MM7.h']]],
+  ['underlying_5ftype_7',['underlying_type',['../classFlags.html#a61795b8383eadabecb8bd837898f74fb',1,'Flags']]],
+  ['ushort_8',['ushort',['../MM7_8h.html#ab95f123a6c9bcfee6a343170ef8c5f69',1,'MM7.h']]]
 ];
