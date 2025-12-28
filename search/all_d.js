54055f8611ccece698e686702d6fbefa6a240055
pskelton <pskelton@users.noreply.github.com>
2025-12-28 16:06:27 +0000

    Deploying to gh-pages from @ pskelton/OpenEnroth@9c1b1f6c224f9fbfb22db662770deaa6ae45d118 🚀

--- search/all_d.js
+++ search/all_d.js
@@ -65,7 +65,7 @@ var searchData=
   ['makegamestate_62',['makeGameState',['../structEngineTraceStateAccessor.html#a9342bd2df2bad6246ca03a4c117e0f8b',1,'EngineTraceStateAccessor']]],
   ['makepalettealpha_63',['MakePaletteAlpha',['../ImageLoader_8cpp.html#a2fb68ea18099b95668487bce26b1e6d7',1,'ImageLoader.cpp']]],
   ['makepalettecolorkey_64',['MakePaletteColorKey',['../ImageLoader_8cpp.html#a3a4cec4da669a8b8d0c01554638ae0c8',1,'ImageLoader.cpp']]],
-  ['makeparticlebillboardandpush_65',['MakeParticleBillboardAndPush',['../classRenderer.html#a365d8bfd43e66071dd147fe6290f585d',1,'Renderer::MakeParticleBillboardAndPush()'],['../classBaseRenderer.html#ad5b2cd584fc7b6cdbaec51951dcbd394',1,'BaseRenderer::MakeParticleBillboardAndPush()']]],
+  ['makeparticlebillboardandpush_65',['MakeParticleBillboardAndPush',['../classRenderer.html#a319892171be5091c626b859bf4e1cf61',1,'Renderer::MakeParticleBillboardAndPush()'],['../classBaseRenderer.html#ab9cbe5077f774393fa30b10b0279c165',1,'BaseRenderer::MakeParticleBillboardAndPush()']]],
   ['makergbaimage_66',['makeRgbaImage',['../ImageFunctions_8h.html#ac5f5b17ca75af1b22f8ab84f3193341f',1,'makeRgbaImage(GrayscaleImageView indexedImage, const Palette &amp;palette):&#160;ImageFunctions.cpp'],['../ImageFunctions_8cpp.html#ac5f5b17ca75af1b22f8ab84f3193341f',1,'makeRgbaImage(GrayscaleImageView indexedImage, const Palette &amp;palette):&#160;ImageFunctions.cpp']]],
   ['makesavelodinfo_67',['makeSaveLodInfo',['../SaveLoad_8cpp.html#a59eca2472eb9c320c2acc2d8527e3dd2',1,'SaveLoad.cpp']]],
   ['makevalidutf8_68',['makeValidUtf8',['../EngineTraceStateAccessor_8cpp.html#aaf0dd43106cd66365bf899ad9ca6069f',1,'EngineTraceStateAccessor.cpp']]],
